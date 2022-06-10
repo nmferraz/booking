33911f7bb9db8f59a102be7acebb5ee3b09fa2b2
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-06-10 23:56:13 +0100

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -2,9 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("MongoDB connected");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+})
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected");
+})
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
@@ -18,5 +36,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8800, () => {
+  connect()
   console.log("Connected to backend.");
 });
